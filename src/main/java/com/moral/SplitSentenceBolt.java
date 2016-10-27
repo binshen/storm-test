ac6a34925e0c7d5a1961a548d864fa563025ff0c
bin.shen <bluelazysb@hotmail.com>
2016-10-27 22:28:00 +0800

    Initial commit

---
+++ src/main/java/com/moral/SplitSentenceBolt.java
@@ -0,0 +1,32 @@
+package com.moral;
+
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+
+import java.util.Map;
+
+public class SplitSentenceBolt extends BaseRichBolt {
+
+    private OutputCollector outputCollector;
+
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+        this.outputCollector = outputCollector;
+    }
+
+    public void execute(Tuple tuple) {
+        String sentence = tuple.getStringByField("sentences");
+        String[] words = sentence.split(" ");
+        for(String word : words){
+            outputCollector.emit(new Values(word));
+        }
+    }
+
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        outputFieldsDeclarer.declare(new Fields("word"));
+    }
+}
